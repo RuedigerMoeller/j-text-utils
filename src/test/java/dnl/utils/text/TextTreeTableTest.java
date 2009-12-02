06f88a48255a42f4c1e7069274702ee9adc18904
daniel.or <daniel.or@67af9080-c46f-11de-a006-439b9ae3df91>
2009-12-02 06:10:31 +0000

    testtreetable + tests

---
+++ src/test/java/dnl/utils/text/TextTreeTableTest.java
@@ -0,0 +1,60 @@
+package dnl.utils.text;
+
+import org.junit.Test;
+
+public class TextTreeTableTest {
+
+	@Test
+	public void printTableWithThreeColumns(){
+		String[] titles = { "cotttumn1", "colu345345mn2", "columnfff3" };
+		String[][] data = { 
+				{ "val10", "val20", "val30" }, 
+				{ "val13", "val20", "val30" }, 
+				{ "val12", "val21", "val31" }, 
+				{ "val11", "val21", "val31" }, 
+				{ "val12", "val25", "val31" }, 
+				{ "val11", "val21", "val31" }, 
+				};
+		TextTreeTable tt = new TextTreeTable(titles, data);
+//		tt.setAddRowNumbering(true);
+//		tt.addSeparatorPolicy(new LastRowSeparatorPolicy());
+		tt.setSort(0);
+		tt.printTable();		
+	}
+
+	@Test
+	public void printTableWithTwoColumns(){
+		String[] titles = { "cotttumn1", "colu345345mn2"};
+		String[][] data = { 
+				{ "val10", "val20"}, 
+				{ "val13", "val20"}, 
+				{ "val12", "val21"}, 
+				{ "val11", "val21"}, 
+				{ "val12", "val25"}, 
+				{ "val11", "val21"}, 
+		};
+		TextTable tt = new TextTable(titles, data);
+//		tt.setAddRowNumbering(true);
+//		tt.addSeparatorPolicy(new LastRowSeparatorPolicy());
+		tt.setSort(0);
+		tt.printTable();		
+	}
+
+	@Test
+	public void printTableWithOneColumn(){
+		String[] titles = { "cotttumn1"};
+		String[][] data = { 
+				{ "val10"}, 
+				{ "val13"}, 
+				{ "val12"}, 
+				{ "val11"}, 
+				{ "val12"}, 
+				{ "val11"}, 
+		};
+		TextTable tt = new TextTable(titles, data);
+//		tt.setAddRowNumbering(true);
+//		tt.addSeparatorPolicy(new LastRowSeparatorPolicy());
+		tt.setSort(0);
+		tt.printTable();		
+	}
+}
