82f4fd6b077d9377ab48f2d7bff5b7c8af237814
daniel.or <daniel.or@67af9080-c46f-11de-a006-439b9ae3df91>
2010-05-09 12:52:13 +0000

    sid

--- trunk/src/main/java/dnl/utils/text/TextTableModel.java
+++ trunk/src/main/java/dnl/utils/text/TextTableModel.java
@@ -4,6 +4,9 @@ import javax.swing.table.AbstractTableModel;
 
 public abstract class TextTableModel extends AbstractTableModel {
 
+	private static final long serialVersionUID = 1L;
+
+
 	public abstract boolean allowNumberingAt(int row);
 	public abstract boolean addSeparatorAt(int row);
 }
